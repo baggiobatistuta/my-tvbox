031339b9010ce24f9f1574748bf8ab9116943131
baggiobatistuta <baggiobatistuta@126.com>
2026-09-17 15:54:21 +0800

    Update check.py

--- check.py
+++ check.py
@@ -261,7 +261,7 @@ def build_multirepo_json(alive):
     """
     urls = []
     myurl = r"https://fastly.jsdelivr.net/gh/baggiobatistuta/my-tvbox@main/output/dancang.json"
-    urls.append(({"name": r"Alex的影视仓","url": myurl,})
+    urls.append({"name": r"Alex的影视仓","url": myurl,})
     
     seen = set()
     SKIP_SUFFIX = (".html", ".php", "/")
